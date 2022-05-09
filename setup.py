b9dde53fea868dca62b83417f1db8fefae9e0f87
Eugenio Zuccarelli <11176606+jayzuccarelli@users.noreply.github.com>
2022-05-09 13:27:01 -0400

    ezuccarelli to jayzuccarelli

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Sparse Regression",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ezuccarelli/sparseregression",
+    url="https://github.com/jayzuccarelli/sparseregression",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
